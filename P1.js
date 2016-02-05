3a0031022031e2207b301ce318c27b3387449153
luxi xu <luck4lucy3@gmail.com>
2016-02-04 19:59:41 -0800

    added video demo

--- P1.js
+++ P1.js
@@ -60,7 +60,7 @@ var grid = new THREE.Line(gridGeometry,gridMaterial,THREE.LinePieces);
 // MATERIALS
 // Note: Feel free to be creative with this! 
 //var normalMaterial = new THREE.MeshNormalMaterial( {color: 0xB7612C, transparent: true, blending: THREE.AdditiveBlending });
-var normalMaterial = new THREE.MeshNormalMaterial(  { shading: THREE.SmoothShading });
+var normalMaterial = new THREE.MeshNormalMaterial(  {color: 0xffaa00, wireframe: true});
 //{ shading: THREE.SmoothShading }
 
 // function drawCube()
